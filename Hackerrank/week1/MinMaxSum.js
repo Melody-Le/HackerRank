8450e5dfe6ab7154149f8ab95df6e8d6fabc0397
melody-le <tanmui1992@gmail.com>
2023-09-02 19:52:14 +0800

    add Grind75

---
+++ Hackerrank/week1/MinMaxSum.js
@@ -0,0 +1,12 @@
+/*
+Given five positive integers, find the minimum and maximum values that can be calculated 
+by summing exactly four of the five integers. 
+Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+*/
+function minMaxSum(arr) {
+  let min = 0;
+  let max = 0;
+  let temp = 0;
+  // find sum of 5  value
+  for ( let i = 0; i)
+}
